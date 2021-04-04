f773fd1c8b4df058877c3b1dbaa66d405d15358a
sosualfred <sosualfred@gmail.com>
2021-04-04 23:06:00 +0000

    add cards and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBasel
 import { PhotoCamera } from '@material-ui/icons';
 import useStyles from './styles';
 
+const cards = [...Array(20).keys()];
 
 const App = () => {
     const classes = useStyles();
@@ -42,7 +43,42 @@ const App = () => {
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                       {cards.map((card) =>  (
+                           <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="primary">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                       ))}
+                    </Grid>
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary">
+                    Something here to give the footer a purpose
+                </Typography>
+            </footer>
         </>
     );
 }
